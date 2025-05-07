d0cd71a8f8bc0b0dbe758bf08d28628d660725ed
Rupanjana Bhattacharjee <rupanjana72@gmail.com>
2025-05-08 01:39:35 +0530

    done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
+window.addEventListener("DOMContentLoaded", function () {
+    VANTA.CLOUDS({
+      el: "#vanta-bg",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      speed: 1.00,
+      backgroundColor: 0xffffff,
+      skyColor: 0xd768b6,
+      cloudColor: 0xadc1de,
+      cloudShadowColor: 0x183550,
+      sunColor: 0xff185d,
+      sunGlareColor: 0xcd32ff,
+      sunlightColor: 0xb326d4
+    });
+  });
+
 document.addEventListener('DOMContentLoaded', function() {
     const display = document.getElementById('display');
     const buttons = document.querySelectorAll('button');
